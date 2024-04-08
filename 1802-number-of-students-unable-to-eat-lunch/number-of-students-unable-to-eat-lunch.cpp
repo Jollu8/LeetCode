19f6a4ab2d583affe159f83f4819aa11ada27ff9
Jollu Emiluulu <zholu8@gmail.com>
2024-04-08 17:50:11 +0300

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,20 +1,15 @@
-
 class Solution {
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int circular = count(students.begin(), students.end(), 0);
-        int square = students.size() - circular;
+    int countStudents(vector<int> &a, vector<int> &b) {
+        int circular = count(a.begin(), a.end(), 0);
+        int square = a.size() - circular;
 
-        auto it =
-            find_if(sandwiches.begin(), sandwiches.end(), [&](int sandwich) {
-                if (sandwich && --square < 0) {
-                    return true;
-                } else if (!sandwich && --circular < 0) {
-                    return true;
-                }
-                return false;
-            });
+        auto it = find_if(b.begin(), b.end(), [&](int s) {
+            if (s && --square < 0)return true;
+            else if (!s && --circular < 0)return true;
+            return false;
+        });
 
-        return it != sandwiches.end() ? distance(it, sandwiches.end()) : 0;
+        return it != b.end() ? distance(it, b.end()) : 0;
     }
 };

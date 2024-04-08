b3ee46c79d4e0318b48a7330365fd22086be1541
Jollu Emiluulu <zholu8@gmail.com>
2024-04-08 13:20:20 +0300

    Time: 2 ms (50.57%) | Memory: 11.2 MB (6.89%) - LeetSync

--- Medium/1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ Medium/1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int countStudents(vector<int>& a, vector<int>& b) {
-        queue<int> q;
-        for (auto i : a)
-            q.push(i);
-
+    int countStudents(vector<int> &a, vector<int> &b) {
+        std::deque<int> q;
+        std::move(a.begin(), a.end(), std::back_inserter(q));
         for (int i = 0; i < b.size();) {
             int n = 0;
             while (n < a.size() && b[i] != q.front()) {
                 auto tmp = q.front();
-                q.pop();
-                q.push(tmp);
+                q.pop_front();
+                q.push_back(tmp);
                 ++n;
             }
             if (n == a.size())
                 break;
             ++i;
-            q.pop();
+            q.pop_front();
         }
         return q.size();
     }

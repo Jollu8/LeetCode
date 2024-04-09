5f62f6b88c98ec3b127b4d331ca7fdceeaf91991
Jollu Emiluulu <zholu8@gmail.com>
2024-04-09 13:30:50 +0300

    Time: 0 ms (100.00%) | Memory: 9.2 MB (99.09%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& n, int k) {
         int x{};
-        for(int i = 0; i < n.size(); ++i) {
-            if(i <= k) x += min(n[k], n[i]);
-            else x += min(n[k]-1, n[i]);
-        }
+        for (int i = 0; i <= k; ++i)
+            x += min(n[k], n[i]);
+
+        for (auto i = k+1; i < n.size(); ++i)
+            x += min(n[k] - 1, n[i]);
         return x;
     }
 };

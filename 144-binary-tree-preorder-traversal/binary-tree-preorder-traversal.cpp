30c12a128e9a23fc01173adbb22d30d80cbacfeb
Jollu Emiluulu <zholu8@gmail.com>
2024-04-10 21:08:34 +0300

    Time: 0 ms (100.00%) | Memory: 9.8 MB (45.75%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,24 +10,39 @@
  * right(right) {}
  * };
  */
+
+//  vector<int> preorderTraversal(TreeNode* r) {
+//         if (!r)
+//             return {};
+//         vector<int> ans;
+
+//         stack<TreeNode*> q;
+//         q.push(r);
+//         while (!q.empty()) {
+//             auto tmp = q.top();
+//             q.pop();
+//             if (tmp->right)
+//                 q.push(tmp->right);
+//             if (tmp->left)
+//                 q.push(tmp->left);
+//             ans.push_back(tmp->val);
+//         }
+//         return ans;
+//     }
 class Solution {
+    void pre_rec(TreeNode *r, vector<int> &ans){
+        if(!r) return;
+        ans.push_back(r->val);
+        pre_rec(r->left, ans);
+        pre_rec(r->right, ans);
+    }
 public:
     vector<int> preorderTraversal(TreeNode* r) {
         if (!r)
             return {};
         vector<int> ans;
 
-        stack<TreeNode*> q;
-        q.push(r);
-        while (!q.empty()) {
-            auto tmp = q.top();
-            q.pop();
-            if (tmp->right)
-                q.push(tmp->right);
-            if (tmp->left)
-                q.push(tmp->left);
-            ans.push_back(tmp->val);
-        }
+        pre_rec(r, ans);
         return ans;
     }
 };

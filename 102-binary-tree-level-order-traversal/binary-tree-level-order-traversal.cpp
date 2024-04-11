aee2547791fcb439b0f8026fdd50b87c3c23d784
Jollu Emiluulu <zholu8@gmail.com>
2024-04-11 12:33:16 +0300

    Time: 10 ms (12.18%) | Memory: 14.8 MB (67.30%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,27 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    int height(TreeNode *r) {
-        if (!r) return 0;
+    int height(TreeNode* r) {
+        if (!r)
+            return 0;
         return 1 + max(height(r->left), height(r->right));
     }
 
-    void levels(TreeNode *r, int level, vector<int> &tmp) {
-        if (!r) return;
-        if (level == 1) tmp.push_back(r->val);
+    void levels(TreeNode* r, int level, vector<int>& tmp) {
+        if (!r)
+            return;
+        if (level == 1)
+            tmp.push_back(r->val);
         else {
             levels(r->left, level - 1, tmp);
             levels(r->right, level - 1, tmp);
         }
     }
-
-public:
-    vector<vector<int>> levelOrder(TreeNode *r) {
-        if (!r) return {};
+    vector<vector<int>> levelOrder_rec(TreeNode* r) {
+        if (!r)
+            return {};
 
         std::vector<std::vector<int>> ans;
         auto h = height(r);
@@ -36,5 +39,28 @@ public:
             ans.emplace_back(tmp);
         }
         return ans;
+    }
+
+public:
+    vector<vector<int>> levelOrder(TreeNode* r){
+        vector<vector<int>> ans;
+        if(!r) return {};
+        queue<TreeNode*> q;
+        q.push(r);
+        while(!q.empty()) {
+            vector<int> buf;
+
+            int size = q.size();
+            for(int _ = 0; _ < size; ++_) {
+                auto tmp = q.front();
+                q.pop();
+                buf.push_back(tmp->val);
+                if(tmp->left)q.push(tmp->left);
+                if(tmp->right)q.push(tmp->right);
+            }
+            ans.emplace_back(buf);
+        }
+        return ans;
+
     }
 };

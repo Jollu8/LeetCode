088443434efc1f52dd8613eedf4a76d8c17a0d34
Jollu Emiluulu <zholu8@gmail.com>
2024-04-13 19:12:54 +0300

    Time: 12 ms (6.70%) | Memory: 18.9 MB (5.06%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,11 +1,18 @@
 class Solution {
+    unordered_map<int, int> mp;
+    int memo(vector<int> &c, int i) {
+        if(i == 0 || i == 1) return c[i];
+        if(mp.contains(i)) return mp[i];
+        return mp[i] = c[i] +min(memo(c, i-1), memo(c, i-2));
+    }
     int dp(vector<int> &c) {
         for(int i = 2; i < c.size(); ++i)c[i] += min(c[i-1], c[i-2]);
         return min(c[c.size()-1], c[c.size()-2]);
     }
 public:
     int minCostClimbingStairs(vector<int>& c) {
-        return dp(c);
+        // return dp(c);
+        return min(memo(c, c.size()-1), memo(c, c.size()-2));
 
     }
 };

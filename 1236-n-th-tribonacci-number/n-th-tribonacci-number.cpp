0e2d5a2ce4da3bbbda97b2c2eb666acb48a3e6a6
Jollu Emiluulu <zholu8@gmail.com>
2024-04-13 18:17:50 +0300

    Time: 0 ms (100.00%) | Memory: 7.9 MB (5.30%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,17 +1,33 @@
 class Solution {
-    int trib_dp(int n ) {
-        vector<int> a(n+1);
-        if(n  < 4) {
+    unordered_map<int, int> mp;
+    int trib_memo(int n) {
+        if (n == 0)
+            return 0;
+        if (n <= 2)
+            return 1;
+
+        if (mp.contains(n))
+            return mp[n];
+        mp[n] =
+            trib_memo(n - 1) + trib_memo(n - 2) + trib_memo(n - 3); return mp[n];
+    }
+
+    int trib_dp(int n) {
+        vector<int> a(n + 1);
+        if (n < 4) {
             a.resize(3);
         }
         a[0] = 0;
         a[1] = 1;
         a[2] = 1;
-        for(int i = 3; i < n+1; ++i) a[i] = a[i-1] + a[i-2] + a[i-3];
+        for (int i = 3; i < n + 1; ++i)
+            a[i] = a[i - 1] + a[i - 2] + a[i - 3];
         return a[n];
     }
+
 public:
-    int tribonacci(int n) {
-        return trib_dp(n);
-    }
+    int tribonacci(int n) { 
+        // return trib_dp(n);
+        return trib_memo(n);
+         }
 };

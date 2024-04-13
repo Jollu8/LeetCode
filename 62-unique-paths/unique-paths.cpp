a15c92cb5e8bee7a179d7fb2c4e390c239eaf96a
Jollu Emiluulu <zholu8@gmail.com>
2024-04-13 22:15:34 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (45.91%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,9 +4,10 @@ public:
         vector<vector<int>> vec(m+1, vector<int>(n+1, 0));
         vec[1][0] = 1;
         for(auto i = 1; i< m+1; ++i)
+            #pragma omp simd
             for(auto j = 1; j < n+1; ++j) {
                 vec[i][j] = vec[i-1][j] + vec[i][j-1];
-        }
+            }
         return vec[m][n];
     }
 };

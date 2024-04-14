2e7c7b21745b094b8ab8c643ce0aba29f1ce0517
Jollu Emiluulu <zholu8@gmail.com>
2024-04-14 11:18:08 +0300

    Time: 11 ms (71.80%) | Memory: 13 MB (92.62%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>> &mat) {
-        vector<vector<int>> dp = mat;
-        for (int i = 1; i < mat.size(); ++i) {
-            for (int j = 0; j < mat[i].size(); ++j) {
+    int minFallingPathSum(vector<vector<int>> &dp) {
+     
+        for (int i = 1; i < dp.size(); ++i) {
+            for (int j = 0; j < dp[i].size(); ++j) {
                 if (j == 0)
                     dp[i][j] += min(dp[i-1][j], dp[i-1][j+1]);
-                else if (j == mat[i].size() - 1)
+                else if (j == dp[i].size() - 1)
                     dp[i][j] += min(dp[i-1][j], dp[i-1][j-1]);
                 else
                     dp[i][j] += min({dp[i-1][j], dp[i-1][j-1], dp[i-1][j+1]});

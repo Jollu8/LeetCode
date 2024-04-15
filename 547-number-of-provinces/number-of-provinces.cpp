67c0df915d70331c9606a7cb7aabd62220fdc66d
Jollu Emiluulu <zholu8@gmail.com>
2024-04-15 17:39:48 +0300

    Time: 19 ms (75.02%) | Memory: 18.1 MB (66.66%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,50 +1,60 @@
-
-class QU {
-    vector<int> root;
-    vector<int> rank;
-
+class UnionFind {
 public:
-    int count;
-    QU(int x) : root(x + 1), count(x) {
-        iota(root.begin(), root.end(), 0);
-        rank = root;
-    }
-
-    int find(int x) {
-        if (x == root[x])
-            return x;
-        return root[x] = find(root[x]);
-    }
-
-    void u_set(int x, int y) {
-        int r_x = find(x);
-        int r_y = find(y);
-        if (r_x != r_y) {
-            if (rank[r_x] > rank[r_y])
-                root[r_y] = r_x;
-            else if (rank[r_x] < rank[r_y])
-                root[r_x] = r_y;
-            else {
-                root[r_y] = r_x;
-                rank[r_x]++;
-            }
-            --count;
+    UnionFind(int sz) : root(sz), rank(sz), count(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
         }
     }
 
-    bool connect(int x, int y) { return find(x) == find(y); }
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            count--;
+        }
+    }
+
+    int getCount() {
+        return count;
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+    int count;
 };
 
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& con) {
-        QU root(con.size() + 1);
-        for (auto i = 0; i < con.size(); ++i) {
-            for (int j = 0; j < con[0].size(); ++j) {
-                if (con[i][j])
-                    root.u_set(i + 1, j + 1);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        if (isConnected.size() == 0) {
+            return 0;
+        }
+        int n = isConnected.size();
+        UnionFind uf(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    uf.unionSet(i, j);
+                }
             }
         }
-        return !(root.count) ? 0 : root.count-1;
+        return uf.getCount();
     }
 };

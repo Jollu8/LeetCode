eedf8f7568a28b6eb8a0dfaad0aceb513a06243d
Jollu Emiluulu <zholu8@gmail.com>
2024-04-16 20:23:18 +0300

    Time: 6 ms (70.28%) | Memory: 13.6 MB (73.60%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,24 +1,44 @@
 class Solution {
     vector<vector<int>> adj;
     vector<bool> visited;
+
 public:
-    bool canVisitAllRooms(vector<vector<int>> &rooms) {
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         visited = vector<bool>(n, false);
         adj = move(rooms);
-        dfs(0);
+        // dfs_rec(0);
+        dfs_iter(0);
 
-        for (int i = 0; i < n; ++i) if (!visited[i]) { return false; }
+        for (int i = 0; i < n; ++i)
+            if (!visited[i]) {
+                return false;
+            }
         return true;
     }
 
-    void dfs(int i) {
-        if (visited[i]) return;
+    void dfs_iter(int i) {
+        stack<int> st;
+        st.push(i);
+
+        while (!st.empty()) {
+            i = st.top();
+            st.pop();
+            visited[i] = true;
+            for (auto v : adj[i])
+                if (!visited[v])
+                    st.push(v);
+        }
+    }
+
+    void dfs_rec(int i) {
+        if (visited[i])
+            return;
         visited[i] = true;
 
-        for (auto v: adj[i]) {
-            if (!visited[v]) dfs(v);
+        for (auto v : adj[i]) {
+            if (!visited[v])
+                dfs_rec(v);
         }
-
     }
 };

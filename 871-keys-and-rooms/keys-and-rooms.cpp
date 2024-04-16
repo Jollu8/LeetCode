8ae6d93cd91a33b2d9e586a4b36efdd5724b9d60
Jollu Emiluulu <zholu8@gmail.com>
2024-04-16 20:38:40 +0300

    Time: 3 ms (96.25%) | Memory: 13.7 MB (50.74%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -8,13 +8,30 @@ public:
         visited = vector<bool>(n, false);
         adj = move(rooms);
         // dfs_rec(0);
-        dfs_iter(0);
+        // dfs_iter(0);
+        bfs_iter(0);
 
         for (int i = 0; i < n; ++i)
             if (!visited[i]) {
                 return false;
             }
         return true;
+    }
+
+    void  bfs_iter(int i ) {
+        queue<int> q;
+        q.push(i);
+        
+
+        while(!q.empty()) {
+            i = q.front();
+            q.pop();
+            visited[i] = true;
+            for(auto v : adj[i]) {
+                if(!visited[v]) q.push(v); 
+            }
+
+        }
     }
 
     void dfs_iter(int i) {

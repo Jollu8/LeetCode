6a1540edb6e0d280ee4ca0a6e85e4e602a57fe9e
Jollu Emiluulu <zholu8@gmail.com>
2024-04-18 15:53:04 +0300

    Time: 189 ms (84.16%) | Memory: 97.1 MB (97.78%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,16 +1,19 @@
+
+
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         vector<int> ans;
         vector<bool> seen(n, false);
         for(auto &v : edges) seen[v[1]] = true;
-        
-        #pragma omp parallel for
-for(int i = 0 ; i < n; ++i) 
-    if(!seen[i]) 
-        #pragma omp critical
-        ans.push_back(i);
-        return ans;
 
+        #pragma omp parallel for
+        for(int i = 0; i < n; ++i) {
+            if(!seen[i]) {
+                #pragma omp critical
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
 };

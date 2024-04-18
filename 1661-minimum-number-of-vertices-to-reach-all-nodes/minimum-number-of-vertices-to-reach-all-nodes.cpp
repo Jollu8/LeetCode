d72b2fb0d63bbb11eb9f35cddc2122091975ae3a
Jollu Emiluulu <zholu8@gmail.com>
2024-04-18 15:45:07 +0300

    Time: 184 ms (90.90%) | Memory: 97 MB (97.78%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,35 +1,16 @@
-#pragma GCC optimize("O3")
 class Solution {
-    int n;
-    vector<vector<int>> edges;
-    vector<int> ans;
-
-    void degree() {
-        vector<int> inDegree(n, 0);
-        for (auto& edge : this->edges)
-            inDegree[edge[1]]++;
-
-        for (int i = 0; i < n; ++i)
-            if (inDegree[i] == 0)
-                ans.push_back(i);
-    }
-
-    void undegree() {
-        vector<bool> seen(n, false);
-        for (auto& v : edges)
-            seen[v[1]] = true;
-#pragma omp parallel for
-        for (int i = 0; i < n; ++i)
-            if (!seen[i])
-#pragma omp critical
-                ans.push_back(i);
-    }
-
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        this->n = n;
-        this->edges = std::move(edges);
-        undegree();
+        vector<int> ans;
+        vector<bool> seen(n, false);
+        for(auto &v : edges) seen[v[1]] = true;
+        
+        #pragma omp parallel for
+for(int i = 0 ; i < n; ++i) 
+    if(!seen[i]) 
+        #pragma omp critical
+        ans.push_back(i);
         return ans;
+
     }
 };

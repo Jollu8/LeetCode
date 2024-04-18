709709646da88e1de481e4ecd7ea6b251fbf91a2
Jollu Emiluulu <zholu8@gmail.com>
2024-04-18 12:17:30 +0300

    Time: 19 ms (25.23%) | Memory: 18.9 MB (15.98%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,26 +1,21 @@
 
 class Solution {
-    vector<bool> vis;
     vector<vector<int>> ans;
 
-    void dfs_memo(vector<vector<int>> &g, vector<int> path, int s, int d) {
-        vis[s] = true;
+    void dfs_memo(vector<vector<int>>& g, vector<int> path, int s, int d) {
         path.push_back(s);
-        if (s == d) ans.emplace_back(path);
+        if (s == d)
+            ans.emplace_back(path);
         else {
-            for (auto v: g[s]) {
-                if (!vis[v]) dfs_memo(g, path, v, d);
-            }
+            for (auto v : g[s])
+                dfs_memo(g, path, v, d);
         }
         path.pop_back();
-        vis[s] = false;
     }
 
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph) {
-        vis = vector<bool>(graph.size(), false);
-        dfs_memo(graph, {}, 0, graph.size()-1);
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        dfs_memo(graph, {}, 0, graph.size() - 1);
         return ans;
-
     }
 };

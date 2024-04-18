c9f5684c8e7212208bb64174def086937b993e2e
Jollu Emiluulu <zholu8@gmail.com>
2024-04-18 21:04:25 +0300

    Time: 8 ms (54.49%) | Memory: 20.2 MB (60.96%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,15 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+
+template <typename T>
+concept IsTreeNode = requires(T a) {
+    { a->left } -> std::convertible_to<T>;
+    { a->right } -> std::convertible_to<T>;
+    { a->val } -> std::convertible_to<int>;
+};
+
 class Solution {
-   
+
 public:
-    bool isValidBST(TreeNode* root, TreeNode *a = nullptr, TreeNode *b = nullptr) {
-        if(!root) return true;
-        if(a && a->val >= root->val || b && b->val <= root->val)return false;
-        return isValidBST(root->left, a, root) && isValidBST(root->right, root, b);
+    template <IsTreeNode T>
+    bool isValidBST(T root, T a = nullptr, T b = nullptr) {
+        if (!root)
+            return true;
+        if (a && a->val >= root->val || b && b->val <= root->val)
+            return false;
+        return isValidBST(root->left, a, root) &&
+               isValidBST(root->right, root, b);
     }
 };

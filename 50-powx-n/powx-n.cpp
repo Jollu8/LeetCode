21adc8f78dc74964a9ecbe08415dfff8cc89998c
Jollu Emiluulu <zholu8@gmail.com>
2024-04-20 15:32:41 +0300

    Time: 2 ms (40.13%) | Memory: 8 MB (45.50%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,15 +1,33 @@
 class Solution {
     double rec_pow(double x, long n) {
-        if(n == 0) return 1;
-        if(n < 0) {
-            n = n*-1;
-            x = 1/x;
+        if (n == 0)
+            return 1;
+        if (n < 0) {
+            n = n * -1;
+            x = 1 / x;
         }
-        if(!(n&1)) return rec_pow(x*x, n/2);
-        else return x*rec_pow(x, n-1);
+        if (!(n & 1))
+            return rec_pow(x * x, n / 2);
+        else
+            return x * rec_pow(x, n - 1);
     }
+
 public:
-    double myPow(double x, int n) {
-        return rec_pow(x, n);
+    double myPow(double x, int nn) {
+        // return rec_pow(x, n);
+        long  n = nn;
+        bool neg = false;
+        if (n < 0)neg = n = -n;
+
+
+        double ans = 1.;
+
+        while (n > 0) {
+            if (n & 1)
+                ans *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return neg ? 1.0 / ans : ans;
     }
 };

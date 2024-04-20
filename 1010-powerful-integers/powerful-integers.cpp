aa6ebbd22bd7e726d122b4701227c0a163024b40
Jollu Emiluulu <zholu8@gmail.com>
2024-04-20 16:56:31 +0300

    Time: 3 ms (44.62%) | Memory: 8 MB (40.00%) - LeetSync

--- 1010-powerful-integers/powerful-integers.cpp
+++ 1010-powerful-integers/powerful-integers.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {
-        if (bound < 2) return {};
+        if (bound < 2)return {};
         int max_i = (x == 1) ? 1 : log(bound) / log(x);
         int max_j = (y == 1) ? 1 : log(bound) / log(y);
-        vector<int> res;
+        set<int>buf;
+
         for (int i = 0; i <= max_i; ++i) {
-            for (int j = 0; j <= max_j && pow(x, i) + pow(y, j) <= bound; ++j) {
-                res.push_back(pow(x, i) + pow(y, j));
-            }
+            for (int j = 0; j <= max_j && pow(x, i) + pow(y, j) <= bound; ++j) 
+                buf.insert(pow(x, i) + pow(y, j));
+
             if (x == 1) break;
         }
-        sort(res.begin(), res.end());
-        res.erase(unique(res.begin(), res.end()), res.end());
-        return res;
+
+        return {buf.begin(), buf.end()};
     }
 };
-

050a6ec4ab83adacfa42318f2b3242d4f63f6238
Jollu Emiluulu <zholu8@gmail.com>
2024-04-23 15:26:04 +0300

    Time: 15 ms (70.48%) | Memory: 17.3 MB (71.33%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
     vector<vector<int>> adj;
-    vector<int >state;
-    vector<int> ans;
+    vector<int> state, ans;
 
     bool dfs(int v) {
         state[v] = 1;
-        for (auto i: adj[v]) if (state[i] == 1 || state[i] == 0 && !dfs(i) ) return false;
+        for (int i: adj[v]) 
+            if (state[i] == 1 || (state[i] == 0 && !dfs(i))) return false;
         state[v] = 2;
         ans.push_back(v);
         return true;
     }
 
 public:
-    vector<int> findOrder(int n, vector<vector<int>> &p) {
-        adj.assign(n, vector<int>());
-        state.assign(n, 0);
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        adj.resize(n);
+        state.resize(n);
 
-        for (auto &e: p)adj[e[0]].push_back(e[1]);
+        for (auto &e: p) adj[e[0]].push_back(e[1]);
 
-        for (auto i = 0; i < n; ++i) if (state[i] == 0 && !dfs(i))return {};
+        for (int i = 0; i < n; ++i) 
+            if (!state[i] && !dfs(i)) return {};
 
         return ans;
-
     }
 };

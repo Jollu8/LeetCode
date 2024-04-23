df660401d41b2ead90fa6a7e45fee3e1e65d0d53
Jollu Emiluulu <zholu8@gmail.com>
2024-04-23 14:14:00 +0300

    Time: 14 ms (75.78%) | Memory: 17.2 MB (66.49%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,29 +3,25 @@ class Solution {
     vector<bool>vis;
     vector<int>ans;
 
-    bool dfs(int v){
+    bool dfs_rec(int v){
         vis[v] = true;
         for(auto i : adj[v]) {
-            if(!vis[i] && dfs(i)) return true;
-            else if(ans[i] == -1) return true;
+            if(!vis[i] && !dfs_rec(i) || ans[i] == -1) return false;
+            // else if(ans[i] == -1) return false;
         }
         ans[v] = 1;
-        return false;
+        return true;
     }
+
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         adj.assign(n, vector<int>());
         vis.assign(n, false);
         ans.assign(n ,-1);
+        for(auto &e : p) adj[e[0]].push_back(e[1]);
+    
+        for(auto i = 0; i < n; ++i) if(!vis[i] && !dfs_rec(i)) return false;
 
-        for(auto &e : p) {
-            adj[e[0]].push_back(e[1]);
-        }
-
-        for(auto i = 0; i < n; ++i) {
-            if(!vis[i] && dfs(i)) return false;
-        }
         return true;
-
     }
 };

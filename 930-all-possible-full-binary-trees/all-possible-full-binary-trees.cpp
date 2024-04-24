af04282caf6cf5a37446d40f9e882225a4eddcb9
Jollu Emiluulu <zholu8@gmail.com>
2024-04-25 00:16:09 +0300

    Time: 71 ms (15.59%) | Memory: 36.2 MB (20.46%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -6,37 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 
-    std::vector<TreeNode*> generateFullBinaryTrees(int n) {
-    if (n % 2 == 0) {
-        return std::vector<TreeNode*>();  }
-
-    if (n == 1) {
-        return {new TreeNode(0)};
-    }
-
-    std::vector<TreeNode*> trees;
-    for (int i = 1; i < n; i += 2) {
-        std::vector<TreeNode*> left_trees = generateFullBinaryTrees(i);
-        std::vector<TreeNode*> right_trees = generateFullBinaryTrees(n - i - 1);
-        for (auto l : left_trees) {
-            for (auto r : right_trees) {
-                TreeNode* current_tree = new TreeNode(0);
-                current_tree->left = l;
-                current_tree->right = r;
-                trees.push_back(current_tree);
-            }
-        }
-    }
-    return trees;
-}
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
-        if(n == 0) return {};
-        return generateFullBinaryTrees(n);
+        if (n % 2 == 0) return std::vector<TreeNode*>();
+        if (n == 1) return {new TreeNode(0)};
+       
+
+        std::vector<TreeNode*> trees;
+        for (int i = 1; i < n; i += 2) {
+            std::vector<TreeNode*> left_trees = allPossibleFBT(i);
+            std::vector<TreeNode*> right_trees = allPossibleFBT(n - i - 1);
+            for (auto l : left_trees) {
+                for (auto r : right_trees) {
+                    TreeNode* current_tree = new TreeNode(0);
+                    current_tree->left = l;
+                    current_tree->right = r;
+                    trees.push_back(current_tree);
+                }
+            }
+        }
+        return trees;
     }
 };

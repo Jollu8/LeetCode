778aac85312b3bc5b568edca620ceea041519bb9
Jollu Emiluulu <zholu8@gmail.com>
2024-04-24 23:12:32 +0300

    Time: 31 ms (5.29%) | Memory: 9.2 MB (5.82%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,6 +1,12 @@
 class Solution {
+   unordered_map<int, int>dp;
 public:
     bool divisorGame(int n) {
-    return !(n&1);
+        if(!dp.contains(n)) dp[n] = 0;
+        if(dp[n] == 0) {
+            for(int i = 1; dp[n] != 1 && i <= n/ 2; ++i) 
+            dp[n] = n%i == 0 ? divisorGame(n-i) ? -1 : 1 : -1;
+        }
+        return dp[n] == 1;
     }
 }; 

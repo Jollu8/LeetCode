f87727c5cfe653275f61c75f9023e5bccecf000a
Jollu Emiluulu <zholu8@gmail.com>
2024-04-27 23:26:00 +0300

    Time: 0 ms (100.00%) | Memory: 7.6 MB (59.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,20 +4,18 @@ public:
         stack<char> st;
 
         for (auto c : s) {
-            if (c == '(' || c == '[' || c == '{')
-                st.push(c);
+            if (c == '(' || c == '[' || c == '{') st.push(c);
             else {
-                if (st.empty())
-                    return false;
-                auto t = st.top();
-                st.pop();
-                if (
-                    c == ']' && t != '[' ||
-                    c == ')' && t != '(' ||
-                    c == '}' && t != '{'
-                ) return false;
+                if (st.empty()) return false;
+                char t = st.top(); st.pop();
+                
+                if (c == ']' && t != '[' ||
+                    c == '}' && t != '{' || 
+                    c == ')' && t != '(')
+                    return false;                
             }
         }
+
         return st.empty();
     }
 };

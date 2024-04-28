feb4fb39c82b9e55fd25ae7570b821ea65d16fed
Jollu Emiluulu <zholu8@gmail.com>
2024-04-28 22:40:45 +0300

    Time: 12 ms (37.83%) | Memory: 13.4 MB (97.17%) - LeetSync

---
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& g) {
+        int n = g.size();
+        vector<vector<int>> a(n-2, vector<int>(n-2));
+
+        for(int i = 0; i < n-2; ++i) {
+            for(int j = 0; j < n-2; ++j) 
+                for(int ii = i; ii < i +3; ++ii) 
+                    for(int jj = j; jj < j + 3; ++jj) a[i][j] = max(a[i][j], g[ii][jj]);
+        }
+        
+        return a;
+        
+    }
+};

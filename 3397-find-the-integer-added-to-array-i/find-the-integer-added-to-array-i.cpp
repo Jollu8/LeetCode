889a131dddd0391d6cb915fd14b7497acbe9d23f
Jollu Emiluulu <zholu8@gmail.com>
2024-04-30 15:02:41 +0300

    Time: 11 ms (31.30%) | Memory: 34.1 MB (86.94%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.cpp
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int addedInteger(vector<int>& n1, vector<int>& n2) {
-        sort(n1.begin(), n1.end());
-        sort(n2.begin(), n2.end());
-        return n2.back() - n1.back();
+        return *min_element(n2.begin(), n2.end()) - *min_element(n1.begin(), n1.end());
     }
 };

49950d98f323700bae6f495190dca0a48763d523
Jollu Emiluulu <zholu8@gmail.com>
2024-04-30 22:14:08 +0300

    Time: 0 ms (100.00%) | Memory: 7 MB (67.64%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool squareIsWhite(string s) {
-        if(!(s[0]&1) && !(s[1]&1)) return false;
-        if ((s[0]&1) && (s[1]&1)) return false;
-        return true;
+        return (s[0]+s[1])%2;
     }
 };

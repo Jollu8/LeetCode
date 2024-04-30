10cf2b8e892b560073bb27e6be7a9c2aa838ba3b
Jollu Emiluulu <zholu8@gmail.com>
2024-04-30 21:45:26 +0300

    Time: 0 ms (100.00%) | Memory: 7.2 MB (88.38%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int findDelayedArrivalTime(int a, int b) {
-        a += b;
-        if(a == 24) return 0;
-        if(a > 24) return a %24;
-        return a;
+        return (a + b) % 24;
         
     }
 };

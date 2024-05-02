2c6f4eb63e3127ccecda47691e84230e08973ea9
Jollu Emiluulu <zholu8@gmail.com>
2024-05-02 16:28:12 +0300

    Time: 3 ms (42.98%) | Memory: 7.4 MB (15.55%) - LeetSync

---
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minTimeToType(string w) {
+        int res = min(w[0] - 'a', 26 - (w[0] - 'a'));
+        for (int i = 1; i < w.size(); ++i)
+            res += min(abs(w[i] - w[i - 1]), 26 - abs(w[i] - w[i - 1]));
+        return res + w.size();
+    }
+};

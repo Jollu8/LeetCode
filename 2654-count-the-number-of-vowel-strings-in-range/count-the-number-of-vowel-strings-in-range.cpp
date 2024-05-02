0123c91bdd31c24c932d7f942bae74e2b5e7dac1
Jollu Emiluulu <zholu8@gmail.com>
2024-05-03 00:14:15 +0300

    Time: 8 ms (86.96%) | Memory: 36.6 MB (5.48%) - LeetSync

--- 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.cpp
+++ 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int vowelStrings(vector<string>& A, int l, int r) {
-        int ans{};
         unordered_set<char> u{'a','o', 'i', 'e', 'u'};
-
-        for(int i = l; i <=r; ++i) {
-            ans += (u.contains(A[i][0]) && u.contains(A[i].back())) ? 1 : 0;
-        }
-        return ans;
+        return count_if(A.begin() + l, A.begin() + r+1, [&](auto &s) {
+            return u.contains(s[0]) && u.contains(s.back());
+        });
         
     }
 };

ffaa6097352f5f702e36252e78b7a0a68722c885
Jollu Emiluulu <zholu8@gmail.com>
2024-05-09 12:28:54 +0300

    Time: 11 ms (36.99%) | Memory: 24.7 MB (80.96%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
         int m = INT_MAX;
-        int index = -1;
+
         for(int i = 0 ; i < nums.size(); ++i){
-            if(int ans = i % 10; ans ==  nums[i] && ans < m) return i;
+            if(int ans = i % 10; ans ==  nums[i]) return i;
         }
-        return index;
+        return -1;
     }
 };

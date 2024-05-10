5cfe3cff00a91244e964b07757850c5d49ef9526
Jollu Emiluulu <zholu8@gmail.com>
2024-05-10 18:49:16 +0300

    Time: 15 ms (56.85%) | Memory: 20.6 MB (10.33%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int candy(vector<int>& s) {
-        int n = s.size();
-        vector<int> v(n, 1);
-        for(int i = 1; i  < n; ++i) v[i] = (s[i] > s[i-1]) ? v[i-1]+1 : v[i];
+    int candy(vector<int> &A) {
+        int n = A.size();
+        vector<int> l(n, 1), r(n, 1);
+        for (int i = 1; i < n; ++i) l[i] = (A[i] > A[i - 1]) ? l[i - 1] + 1 : l[i];
+        for (int i = n - 2; i >= 0; --i) r[i] = (A[i] > A[i + 1]) ? r[i + 1] + 1 : r[i];
+        return inner_product(l.begin(), l.end(), r.begin(), 0, plus<>(), [](int a, int b) {
+            return max(a, b);
+        });
 
-        for(int i = n -2; i>=0; --i) v[i] = (s[i]  > s[i+1]) ? max(v[i], v[i+1]+1) : v[i];
-
-        return accumulate(v.begin(), v.end(), 0);
     }
 };
+

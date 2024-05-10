7da628cf5f5355a6af78d5e333e07324efd75c69
Jollu Emiluulu <zholu8@gmail.com>
2024-05-10 23:26:56 +0300

    Time: 27 ms (66.36%) | Memory: 15.6 MB (99.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    vector<vector<char>> adj;
+    vector<vector<char>>adj;
     int r,c;
+
     void dfs(int i, int j) {
-        if(i < 0 || j < 0 || i>=r || j >= c || adj[i][j] == '0' )return;
+        if(i < 0 || j < 0 || i>= r || j >= c || adj[i][j] == '0') return ;
         adj[i][j] = '0';
         dfs(i-1, j);
         dfs(i+1, j);
         dfs(i, j-1);
-        dfs(i, j+1);
+        dfs(i, j+1); 
     }
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        adj = move(grid);
-        this->r = adj.size(), this->c = adj[0].size();
+    int numIslands(vector<vector<char>>& g) {
+        adj = move(g);
+        r = adj.size(), c = adj[0].size();
         int ans{};
-
         for(int i = 0; i < r; ++i) {
-            for(auto j = 0; j < c; ++j) {
-                if(adj[i][j] == '1'){
+            for(int j = 0; j  < c; ++j) {
+                if(adj[i][j] == '1') {
                     dfs(i, j);
                     ++ans;
                 }
             }
         }
         return ans;
-        
     }
 };

4cef100ac7b1b0824fc8c0d24495ba8d78e1e9a6
Jollu Emiluulu <zholu8@gmail.com>
2024-05-10 23:59:50 +0300

    Time: 6 ms (62.11%) | Memory: 8 MB (94.52%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> A(127);
-        vector<int> B(127);
-        int n = s.size();
-
-        for (int i = 0; i < n; ++i) {
-            if (A[s[i]] != B[t[i]])
-                return false;
+        vector<int> A(127), B(127);
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (A[s[i]] != B[t[i]]) return false;
             A[s[i]] = i + 1;
             B[t[i]] = i + 1;
         }

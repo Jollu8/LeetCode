05bab69d50dc1f93c0045c85135a136ebb31159e
Jollu Emiluulu <zholu8@gmail.com>
2024-05-10 23:38:20 +0300

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.42%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> ma, mb;
-        for(int i = 0; i < s.size(); ++i) {
-            if(ma[s[i]] && ma[s[i]] != t[i]) return false;
-            if(mb[t[i]] && mb[t[i]] != s[i]) return false;
-            ma[s[i]] = t[i];
-            mb[t[i]] = s[i];
+        unordered_map<char, char> m;
+        unordered_set<char> used;
+        for(int i=0; i<s.size(); i++){
+            if (!m.contains(s[i])){
+                if (used.count(t[i])==1)
+                    return false;
+                m[s[i]] = t[i];
+                used.insert(t[i]);
+            }else if(m[s[i]]!=t[i])return false;
         }
         return true;
     }

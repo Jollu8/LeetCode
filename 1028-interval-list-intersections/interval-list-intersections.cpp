f4074d4b2fcc1e8e3efaf1bd9be600ae47e59c5f
Jollu Emiluulu <zholu8@gmail.com>
2024-05-11 14:28:45 +0300

    Time: 24 ms (51.73%) | Memory: 22.3 MB (37.53%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,13 +1,13 @@
 class Solution {
+    using vvi = vector<vector<int>>;
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>> ans;
-        for(int i{}, j{}; i < A.size() && j < B.size(); A[i][1]  < B[j][1] ? ++i :  ++j) {
-            auto s  = max(A[i][0], B[j][0]);
+    auto intervalIntersection(vvi& A, vvi& B) {
+        vvi ans;
+        for (int i{}, j{}; i < A.size() && j < B.size(); A[i][1] < B[j][1] ? ++i : ++j) {
+            auto s = max(A[i][0], B[j][0]);
             auto e = min(A[i][1], B[j][1]);
-            if(s <= e) ans.push_back({s, e});
+            if (s <= e) ans.push_back({s, e});
         }
         return ans;
-        
     }
 };

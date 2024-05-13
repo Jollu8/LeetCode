c19d1da55cab476c0af75eff2e7af55f2646d5c0
Jollu Emiluulu <zholu8@gmail.com>
2024-05-13 20:33:36 +0300

    Time: 2 ms (56.18%) | Memory: 8.5 MB (35.71%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestGoodInteger(string s) {
         string ans;
         for(auto i = 2; i < s.size(); ++i) {
-            if(s[i-1] == s[i-2] && s[i] == s[i-1] && s[i] == s[i-2]) ans = max(ans,  string() + s[i-2] + s[i-1] + s[i]);
+            if(s[i-1] == s[i-2] && s[i] == s[i-1] ) ans = max(ans,  string() + s[i-2] + s[i-1] + s[i]);
         }
         return ans;
     }

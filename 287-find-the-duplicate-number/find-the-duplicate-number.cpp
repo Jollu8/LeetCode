c36754e5fe04a1dfba6469170edb6ee0b5b79ac8
Jollu Emiluulu <zholu8@gmail.com>
2024-05-16 23:43:08 +0300

    Time: 74 ms (75.94%) | Memory: 66.1 MB (22.49%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int findDuplicate(vector<int>& A) {
         vector<int>seen(A.size()+1);
-        for(auto i : A) {
-            ++seen[i];
-            if(seen[i] > 1)return i;
-        }
-        return 42;
-        
+        for(auto i : A) 
+            if(++seen[i] > 1)return i;
+
+        return 42;      
     }
 };

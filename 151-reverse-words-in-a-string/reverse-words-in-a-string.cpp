012641c247b08d0358400865f9f878a9212f9425
Jollu Emiluulu <zholu8@gmail.com>
2024-05-16 22:13:33 +0300

    Time: 7 ms (36.32%) | Memory: 23.4 MB (11.98%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
+        string ans, tok;
+        ans.reserve(s.size());
         stringstream ss(s);
-        string tok;
-        string ans;
-        while (ss >> tok) 
-            ans = tok + " " + ans;
+        while (ss >> tok) ans  = tok + " "  + ans;
         
         ans.pop_back();
-        return ans;  // Возвращает обработанную строку, а не исходную
+        return ans;
     }
 };

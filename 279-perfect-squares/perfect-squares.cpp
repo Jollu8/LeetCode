2cb1df3a33b0ad402f1382abdb6e2d0d86a578e0
Jollu Emiluulu <zholu8@gmail.com>
2024-05-17 22:22:40 +0300

    Time: 54 ms (56.34%) | Memory: 13.1 MB (15.02%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    int numSquares(int n) {
-        queue<int> q({n});
-        int ans{};
-
-        while (!q.empty()) {
-            int size = q.size();
-            ans++;
-            while (size--) {
-                int cur = q.front();
-                q.pop();
-                for (int i = 0; i * i <= cur; ++i) {
-                    int sub = cur - i * i;
-                    if (!sub)
-                        return ans;
-                    q.push(sub);
-                }
-            }
-        }
-        return 0;
+    int numSquares(int n, int m = 1) {
+        vector<long long> dp(max(m, n+1), INT_MAX);
+        dp[0] = 0;
+        for(int i{}, ii; (ii = i*i) <= n; ++i) 
+            for(int j = max(m, ii); j<=n; ++j)
+                dp[j] = (dp[j] > dp[j-ii]+1) ? dp[j -ii] + 1 : dp[j];
+        return dp[n];
     }
 };

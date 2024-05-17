3dc8f47e8cac10aa29f7414ddb864f87852c2dbe
Jollu Emiluulu <zholu8@gmail.com>
2024-05-17 23:27:32 +0300

    Time: 9 ms (37.74%) | Memory: 15.4 MB (56.11%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,25 +1,21 @@
-#define S size()
-#define PB push_back
-
 class Solution {
 public:
-    int evalRPN(vector<string>& t) {
-        vector<int> st;
-        unordered_map<char, function<int(int, int)>> ops {
-                {'+', [](int a, int b) { return a + b; }},
-                {'-', [](int a, int b) { return a - b; }},
-                {'*', [](int a, int b) { return a * b; }},
-                {'/', [](int a, int b) { return a / b; }}
+    int evalRPN(vector<string>& S) {
+        unordered_map<char, function<int(int, int)>> func {
+            {'+', [](int a, int b){return a + b;}},
+            {'-', [](int a, int b){return a - b;}},
+            {'*', [](int a, int b){return a * b;}},
+            {'/', [](int a, int b){return a / b;}}
         };
-        for (auto i : t) {
-            if (ops.count(i.back())) {
-                int a = st.back(); st.pop_back();
-                int b = st.back(); st.pop_back();
-                st.PB(ops[i.back()](b, a));
-            } else {
-                st.PB(stoi(i));
-            }
+        stack<int>st;
+
+        for(auto &c : S) {
+            if(!isdigit(c.back())){
+                auto b = st.top(); st.pop();
+                auto a = st.top(); st.pop();
+                st.push(func[c.back()](a, b));
+            }else st.push(stoi(c));
         }
-        return st.back();
+        return st.top();
     }
 };

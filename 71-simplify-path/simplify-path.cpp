9bcce353a06b0b95fb9b10f3683cc656c05e0a61
Jollu Emiluulu <zholu8@gmail.com>
2024-05-17 18:30:20 +0300

    Time: 9 ms (21.78%) | Memory: 11.7 MB (25.36%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    string simplifyPath(string path) {
-        stringstream ss(path);
+    string simplifyPath(string s) {
+        using namespace std::string_literals;
+        stringstream ss(s);
         stack<string> st;
-        string ans, dir;
-        while(getline(ss, dir, '/')) {
-            if(dir.empty() ||  dir == "." )continue;
-            else if( dir == "..") {if(!st.empty()) st.pop();}
-            else st.push(dir);
+        string ans, tok;
+
+        while (getline(ss, tok, '/')) {
+            if (tok.empty() || tok == "."s) continue;
+            else if (tok == ".."s) { if (!st.empty()) st.pop(); }
+            else st.push(tok);
         }
-        while(!st.empty()){
-            ans = "/" + st.top() + ans;
-            st.pop();
-        }
+
+        for (; !st.empty(); st.pop()) ans  = "/" +  st.top() + ans;
         return ans.empty() ? "/" : ans;
     }
 };

5276cc5094870a3a25227f3371a3ab7f3adf1298
Jollu Emiluulu <zholu8@gmail.com>
2024-05-18 16:16:01 +0300

    Time: 3 ms (63.77%) | Memory: 12.8 MB (75.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,15 +1,13 @@
-class Solution {
-    int n;
-    vector<string>ans;
-    void backtrack(int l , int r, string cur){
-        if(cur.size() == n*2)ans.emplace_back(cur);
-        if(l < n) backtrack(l+1, r, cur + "(");
-        if(r < l) backtrack(l, r+1, cur + ")");
-    }
+class Solution {    
 public:
     vector<string> generateParenthesis(int n) {
-        this->n = n;
-        backtrack(0, 0, "");
+        vector<string>ans;
+        function<void(int, int, string)> func = [&](int l, int r, string s) {
+            if(s.size() == n *2) ans.emplace_back(s);
+            if(l < n) func(l+1, r, s + "(");
+            if(r < l) func(l, r+1, s + ")");
+        };
+        func(0, 0, "");
         return ans;
-    }
+     }
 };

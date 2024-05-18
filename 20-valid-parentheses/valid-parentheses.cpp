eb4768639b7665910c93b3373b0af073bbdf25ca
Jollu Emiluulu <zholu8@gmail.com>
2024-05-18 22:49:27 +0300

    Time: 0 ms (100.00%) | Memory: 7.4 MB (85.62%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for (auto c : s) {
-            if (c == '(' || c == '[' || c == '{') st.push(c);
-            else {
-                if (st.empty()) return false;
-                char t = st.top(); st.pop();
-                
-                if (c == ']' && t != '[' ||
-                    c == '}' && t != '{' || 
-                    c == ')' && t != '(')
-                    return false;                
+        
+        for(auto c : s) {
+            if(c == '(' || c == '{' || c == '[')st.push(c);
+            else{
+                if(st.empty()) return false;
+                auto t = st.top();
+                st.pop();
+                if(t == '[' && c != ']' ||
+                    t == '{' && c != '}' ||
+                    t == '(' && c != ')') return false;
             }
         }
-
         return st.empty();
     }
 };

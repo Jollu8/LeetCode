85b1a5bad52e290de3ceb4d6885f541496340408
Jollu Emiluulu <zholu8@gmail.com>
2024-05-18 18:20:18 +0300

    Time: 25 ms (71.73%) | Memory: 21.6 MB (99.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,15 +1,20 @@
+int speed = [] {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0; }();
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string> &s) {
+    vector<vector<string>> groupAnagrams(vector<string>& s) {
         unordered_map<string, int> mp;
         vector<vector<string>> ans(s.size());
         int index{};
-        for (auto &w: s) {
+        for (auto& w : s) {
             auto tmp = w;
             sort(w.begin(), w.end());
             if (auto f = mp.find(w); f != mp.end())
                 ans[f->second].emplace_back(tmp);
-            else ans[mp[w] = index++].emplace_back(tmp);
+            else
+                ans[mp[w] = index++].emplace_back(tmp);
         }
         ans.resize(index);
         return ans;

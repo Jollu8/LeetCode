6f7fc0bd3c51ce102b9060fd4b3f78d8ea492cab
Jollu Emiluulu <zholu8@gmail.com>
2024-05-19 22:51:24 +0300

    Time: 7 ms (88.19%) | Memory: 9.9 MB (68.48%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,17 +1,22 @@
+
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> a(26), b(26), ans;
         int n = s.size(), m = p.size();
         if(n < m) return {};
         for(int i = 0; i < m; ++i) {
-            a[p[i] - 'a']++;
-            b[s[i] - 'a']++;
+            ++a[p[i] - 'a'];
+            ++b[s[i] - 'a'];
         }
         if(a == b)ans.push_back(0);
         for(int i = m; i < n; ++i) {
-            b[s[i-m] - 'a']--;
-            b[s[i] - 'a']++;
+            --b[s[i-m] - 'a'];
+            ++b[s[i] - 'a'];
+
             if(a == b) ans.push_back(i-m+1);
         }
         return ans;

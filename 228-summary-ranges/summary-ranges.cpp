03347efcba618233e06fc8a8a0073260efc811ce
Jollu Emiluulu <zholu8@gmail.com>
2024-05-23 22:02:54 +0400

    Time: 0 ms (100.00%) | Memory: 8 MB (99.24%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    vector<string> summaryRanges(vector<int>& A) {
+    vector<string> summaryRanges(vector<int> &A) {
+        using namespace std::string_literals;
         vector<string> ans;
-        for(int l = 0, r = 0; r < A.size(); ++r) {
-            if(r+1 == A.size() || A[r]+1 != A[r+1]){
-                ans.emplace_back(l != r ? to_string(A[l]) + "->" + to_string(A[r]) : to_string(A[r]));
-                l = r+1;
-            }
+        for (int l = 0, r = 0; r < A.size();) {
+            while (r + 1 < A.size() && A[r] + 1 == A[r + 1])++r;
+            if (r - l > 0)
+                ans.emplace_back(to_string(A[l]) + "->"s + to_string(A[r++]));
+            else ans.emplace_back(to_string(A[r++]));
+            l = r;
         }
         return ans;
+
     }
 };

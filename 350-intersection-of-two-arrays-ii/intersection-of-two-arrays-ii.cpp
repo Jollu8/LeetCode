3a33cd9c480da93c55296e1144524fc46f7b132b
Jollu Emiluulu <zholu8@gmail.com>
2024-05-23 20:13:29 +0400

    Time: 3 ms (77.88%) | Memory: 12.4 MB (90.89%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& v1, vector<int>& v2) {
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
-        auto vit1 = v1.begin();
-        auto vit2 = v2.begin();
-
-        while (vit1 != v1.end() && vit2 != v2.end()) {
-            if (*vit1 < *vit2)
-                ++vit1;
-            else if (*vit1 > *vit2)
-                ++vit2;
-            else {
-                ans.push_back(*vit1);
-                ++vit1;
-                ++vit2;
+        if(nums2.empty() || nums1.empty()) return ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        auto a = nums1.begin();
+        auto b = nums2.begin();
+        
+        while(a != nums1.end() && b != nums2.end()){
+            if(*a < *b) ++a;
+            else if(*a > *b) ++b;
+            else{
+                ans.push_back(*a);
+                ++a;
+                ++b;
             }
         }
         return ans;

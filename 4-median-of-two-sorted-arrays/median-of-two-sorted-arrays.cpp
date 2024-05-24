54fa9b0d6b4bbd64fc539d57fa35ce083896376a
Jollu Emiluulu <zholu8@gmail.com>
2024-05-25 00:10:52 +0300

    Time: 23 ms (69.90%) | Memory: 95 MB (43.60%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& n1, vector<int>& n2) {
-        vector<int> buf(n1.size() + n2.size());
-        int l{},r{};
-        int index{};
-        while(l < n1.size() && r < n2.size()) {
-            
-            buf[index++] = (n1[l] < n2[r]) ? n1[l++] : n2[r++];
-        }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) { 
+        int n1 = nums1.size(), n2 = nums2.size();
+        vector<int> buf(n1+n2);
+        int l{}, r{},i{};
+        while(l < n1 && r < n2)
+            buf[i++] = nums1[l] > nums2[r] ? nums2[r++] : nums1[l++];
+        while(l < n1) buf[i++] = nums1[l++];
+        while(r < n2) buf[i++] = nums2[r++];
+        
+        int m = buf.size() / 2;
+        return buf.size()&1 ? buf[m] : (buf[m-1] + buf[m]) / 2.;
 
-        while(l < n1.size()) buf[index++] = n1[l++];
-        while(r < n2.size()) buf[index++] = n2[r++];
-
-        int m = buf.size()/2;
-
-        if(buf.size()&1) return buf[m];
-        return (buf[m-1] + buf[m])/(double)2;
     }
 };

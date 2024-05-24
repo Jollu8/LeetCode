0c0c46131d49964408492d9d3b06df9a161d9952
Jollu Emiluulu <zholu8@gmail.com>
2024-05-24 16:52:42 +0400

    Time: 8 ms (34.68%) | Memory: 8.5 MB (96.67%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for(int l{}, r = s.size()-1; l <=r; ++l, --r) {
+        for (int l{}, r = s.size() - 1; l < r; ++l, --r){
             if(!isalnum(s[r]))--l;
             else if(!isalnum(s[l]))++r;
-            else if(tolower(s[l]) != tolower(s[r])) return false;
-        }
+            else if (tolower(s[l]) != tolower(s[r])) return false;
+        } 
         return true;
     }
 };

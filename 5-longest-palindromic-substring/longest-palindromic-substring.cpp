3618b5c549dc8f5f5d6c3a6d175d1f172240285a
Jollu Emiluulu <zholu8@gmail.com>
2024-05-24 23:27:09 +0300

    Time: 1312 ms (5.81%) | Memory: 481.7 MB (6.37%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool is_palin(string& s) {
+    bool is_palin(string &s) {
         for (int l = 0, r = s.size() - 1; l < r && r > l; ++l, --r) {
             if (s[l] != s[r])return false;
         }
@@ -9,19 +9,15 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         string ans;
-        for (int l = 0; l < s.size() - 1; ++l) {
-            for (int r = l + 1; r < s.size(); ++r) {
-                if (s[l] == s[r]) {
-                    auto tmp = s.substr(l, r - l + 1);
-                    if (is_palin(tmp) && ans.size() < tmp.size())
-                        ans = tmp;
+        for (int i = 0; i < s.size()-1; ++i) {
+            for (int j = i + 1; j < s.size(); ++j) {
+                if (s[i] == s[j]) {
+                    auto tmp = s.substr(i, j - i + 1);
+                    if (ans.size() < tmp.size() && is_palin(tmp)) ans = tmp;
                 }
             }
         }
-        if (ans.empty()) {
-            ans = s[0];
-            return ans;
-        }
+        if (ans.empty()) return {s[0]};
         return ans;
     }
 };

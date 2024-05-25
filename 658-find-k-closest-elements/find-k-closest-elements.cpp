07ed6d58c53345acd1d086d18f5e3422c1886127
Jollu Emiluulu <zholu8@gmail.com>
2024-05-25 16:20:42 +0300

    Time: 25 ms (81.45%) | Memory: 33.6 MB (86.28%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,19 +1,12 @@
-
 class Solution {
 public:
     vector<int> findClosestElements(vector<int> &arr, int k, int x) {
-        int R = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
-        int L = R - 1;
-
-        vector<int> ans;
-        while (k--) {
-            if (R < arr.size() && (L < 0 || arr[R] - x < x - arr[L]))
-                ans.push_back(arr[R++]);
-            else
-                ans.push_back(arr[L--]);
+        int l{}, r = arr.size() - 1;
+        while(r-l >= k){
+            if(abs(arr[l] - x) > abs(arr[r] - x))++l;
+            else --r;
         }
+        return {arr.begin() + l, arr.begin() + r+1};
 
-        std::sort(ans.begin(), ans.end());
-        return ans;
     }
 };

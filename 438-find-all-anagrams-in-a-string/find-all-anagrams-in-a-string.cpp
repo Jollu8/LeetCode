d38878db725a1e749a7bb48a46793d46ce0a80ad
Jollu Emiluulu <zholu8@gmail.com>
2024-05-26 20:27:49 +0300

    Time: 0 ms (100.00%) | Memory: 9.6 MB (99.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> mp(26), ans;
-        vector<int> buf = mp;
-        for(auto c : p) mp[c-'a']++;
+    vector<int> findAnagrams(string &s, string &p) {
+        vector<int> dp(26), ans;
+        for(auto c : p)dp[c-'a']++;
 
-        for(int l = 0, r = 0; r < s.size(); ++r) {
+        for(int l{}, r{}; r < s.size(); ++r) {
             int i = s[r] - 'a';
-            mp[i]--;
-            for(; mp[i] < 0; mp[s[l++] - 'a']++);
-            if(r- l + 1 == p.size())ans.push_back(l);
+            dp[i]--;
+            for(;dp[i] < 0; dp[s[l++] - 'a']++);
+            if(r - l + 1 == p.size())ans.push_back(l);
         }
         return ans;
     }

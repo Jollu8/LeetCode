58d708ac9bffa3f2a74fe32e9fb868852d95f071
Jollu Emiluulu <zholu8@gmail.com>
2024-05-26 20:07:50 +0300

    Time: 0 ms (100.00%) | Memory: 9.7 MB (97.97%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,16 @@
-
 class Solution {
 public:
-    vector<int> findAnagrams(string &s, string &p) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<int> a(26), b(26), ans;
-        int n = s.size(), m = p.size();
-        if(n < m) return {};
-        for(int i = 0; i < m; ++i) {
-            ++a[p[i] - 'a'];
-            ++b[s[i] - 'a'];
-        }
-        if(a == b)ans.push_back(0);
-        for(int i = m; i < n; ++i) {
-            --b[s[i-m] - 'a'];
-            ++b[s[i] - 'a'];
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> mp(26), ans;
+        vector<int> buf = mp;
+        for(auto c : p) mp[c-'a']++;
 
-            if(a == b) ans.push_back(i-m+1);
+        for(int l = 0, r = 0; r < s.size(); ++r) {
+            int i = s[r] - 'a';
+            mp[i]--;
+            for(; mp[i] < 0; mp[s[l++] - 'a']++);
+            if(r- l + 1 == p.size())ans.push_back(l);
         }
         return ans;
-
     }
 };

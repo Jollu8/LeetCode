f13f137b341e910b7131d2112a84b3fc21871556
Jollu Emiluulu <zholu8@gmail.com>
2024-05-30 23:55:42 +0300

    Time: 63 ms (8.94%) | Memory: 21 MB (86.72%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    bool validPalindrome(string s) {
+    bool validPalindrome(string s, int l1 = 0, int r1 = 0, int r2 = 0,  int l2 = 0) {
         
        for(int l = 0, r = s.size()-1, cnt = 1; l < r;++l, --r) {
         
            if(s[l] != s[r]) {
-            int l1 = 0, r1 = 0,  r2 = 0,  l2 = 0;
                for(l1 = l, r1 = r-1; l1 < r1 && s[l1] == s[r1]; ++l1, --r1);
                for(l2 = l+1, r2 = r; l2 < r2 && s[l2] == s[r2]; ++l2, --r2);
                return l2 >= r2 or l1 >= r1;

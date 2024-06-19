3c3d1f55ed75f41f1cf3f1b7e5810bc450f4285c
Jollu Emiluulu <zholu8@gmail.com>
2024-06-19 18:09:37 +0300

    Time: 3 ms (98.03%) | Memory: 17.8 MB (99.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& A, int t) {
-        int n = A.size(), l{}, r = A.size()-1, cur;
-        for (; (cur = A[l] + A[r]) != t ; cur < t ? ++l : --r);
+        int l{}, r = A.size()-1;
+        for (int cur; (cur = A[l] + A[r]) != t ; cur < t ? ++l : --r);
         return {l+1, r+1};
     }
 };

3b0df6afe063873d8c94a8bda6bc68bfddc5ac1c
Jollu Emiluulu <zholu8@gmail.com>
2024-06-20 20:24:23 +0300

    Time: 0 ms (100.00%) | Memory: 7.1 MB (24.84%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -3,7 +3,6 @@ public:
     bool judgeSquareSum(int c) {
         long long cur;
         for(long long l = 0, r = sqrt(c); (cur = l*l + r*r) != c && l <= r; cur < c ? ++l : --r);
-        
         return cur == c;
     }
 };

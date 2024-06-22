542e2d1b46afa46a5b45f9b39c0bd38a100d903b
Jollu Emiluulu <zholu8@gmail.com>
2024-06-22 20:21:14 +0300

    Time: 3 ms (29.65%) | Memory: 7.4 MB (32.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,28 +1,11 @@
 class Solution {
-    // unordered_map<int, int> mp;
-
-    // int rec_memoziation(int n) {
-    //     if (mp.contains(n)) return mp[n];
-
-    //     int ans;
-    //     if (n == 0 || n == 1) ans = 1;
-    //     else ans = rec_memoziation(n - 1) + rec_memoziation(n - 2);
-    //     mp[n] = ans;
-    //     return ans;
-    // }
-
-    int dp(int n) {
-        if (n < 2) return 1;
-        vector<int> ans(n + 1);
-        ans[0] = 1;
-        ans[1] = 1;
-
-        for (int i = 2; i < ans.size(); i++) ans[i] = ans[i-1] + ans[i - 2];
-        return ans[n];
-    }
-
 public:
     int climbStairs(int n) {
-        return dp(n);
+        if(n < 4) return n;
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(auto i = 2; i <=n ;++i) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };

00be7145c52972251d9bef293e5798930c54134b
Jollu Emiluulu <zholu8@gmail.com>
2024-06-24 15:54:24 +0300

    Time: 37 ms (86.99%) | Memory: 13.1 MB (18.02%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int numSquares(int n, int m = 1) {
-        vector<long long> dp(max(m, n+1), INT_MAX);
+    int numSquares(int n) {
+        vector<long long> dp(n+1, 10e9);
         dp[0] = 0;
-        for(int i{}, ii; (ii = i*i) <= n; ++i) 
-            for(int j = max(m, ii); j<=n; ++j)
+
+        for(int i{}, ii; (ii = i*i) <= n; ++i)
+            for(int j = max(1, ii); j <= n; ++j)
                 dp[j] = min(dp[j], dp[j-ii]+1);
         return dp[n];
+
+        
     }
 };

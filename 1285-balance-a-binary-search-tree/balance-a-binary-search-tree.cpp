25e55c231714a892646770c9d76e66e1b8b0933a
Jollu Emiluulu <zholu8@gmail.com>
2024-06-26 15:33:09 +0300

    Time: 109 ms (25.82%) | Memory: 62.5 MB (60.81%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -42,14 +42,3 @@ public:
         return balanceBST(0, arr.size()-1);
     }
 };
-
-
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();

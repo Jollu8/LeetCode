86f8e1343fdc73ab77a90b8d2784230fdf64a79a
Jollu Emiluulu <zholu8@gmail.com>
2024-06-27 15:10:53 +0300

    Time: 3 ms (88.79%) | Memory: 10.1 MB (90.59%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,26 +1,28 @@
 class Solution {
     vector<vector<int>> ans;
-    void backtrack(vector<int>&vec, int n) {
-        if(n== vec.size()-1){
-            ans.emplace_back(vec);
-        return;
+    unordered_map<int, int> mp;
+    vector<int> nums;
+    void dfs(vector<int> &dp) {
+        if(dp.size() == nums.size()) {
+            ans.emplace_back(dp);
+            return;
         }
 
-        unordered_set<int> seen;
-
-        for(int i = n; i < vec.size(); ++i) {
-            if(!seen.contains(vec[i])){
-            swap(vec[i], vec[n]);
-            backtrack(vec, n+1);
-            swap(vec[i], vec[n]);
-            seen.insert(vec[i]);
-            }
+        for(auto &[a, freq] : mp) {
+            if(!freq) continue ;
+            --freq;
+            dp.push_back(a);
+            dfs(dp);
+            dp.pop_back();
+            ++freq;
         }
-
     }
 public:
-    vector<vector<int>> permuteUnique(vector<int>& vec) {
-        backtrack(vec, 0);
+    vector<vector<int>> permuteUnique(vector<int>& A) {
+        for(auto i  : A) mp[i]++;
+        this->nums = move(A);
+        vector<int> dp;
+        dfs( dp);
         return ans;
     }
 };

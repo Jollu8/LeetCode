d258530c623fcd6875d3463d704e598e1d15a15c
Jollu Emiluulu <zholu8@gmail.com>
2024-06-27 21:58:38 +0300

    Time: 18 ms (29.17%) | Memory: 14.5 MB (43.35%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,44 +1,31 @@
 class Solution {
     vector<vector<int>> ans;
-    vector<vector<int>> g;
-    vector<int> path;
+    vector<int> dp;
+    vector<vector<int>> G;
 
-    void dfs_memo(int s, int d) {
-        path.push_back(s);
-        if (s == d)
-            ans.emplace_back(path);
-        else {
-            for (auto v: g[s])
-                dfs_memo(v, d);
+    inline void bkg(int ind, int v) {
+        cout << G.size();
+        dp.push_back(ind);
+
+        if (ind == v) {
+            ans.push_back(dp);
+            dp.pop_back();
+            return;
         }
-        path.pop_back();
-    }
 
-    void dfs_st(int d) {
-        stack<pair<int, vector<int>>> st;
-        st.push({0, {0}});
-        while (!st.empty()) {
-            auto [u, path_] = st.top();
-            st.pop();
-            if (u == d) ans.emplace_back(path_);
-            else {
-                for (auto v: g[u]) {
-                    vector<int> newPath = path_;
-                    newPath.push_back(v);
-                    st.push({v, newPath});
-                }
-            }
+        for (auto i : G[ind]){
+            bkg(i, v);
+       
         }
+        dp.pop_back();
     }
-
 
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph) {
-        int n = (int) graph.size();
-        path.reserve(n);
-        this->g = std::move(graph);
-//        dfs_memo(0, n - 1);
-        dfs_st(n - 1);
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+
+        G = move(graph);
+        int n = G.size();
+        bkg(0, n - 1);
         return ans;
     }
 };

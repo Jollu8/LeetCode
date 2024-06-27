58d93c779d04555fe4436cba84d71714d848239b
Jollu Emiluulu <zholu8@gmail.com>
2024-06-27 18:57:30 +0300

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.81%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,15 +1,34 @@
 class Solution {
-public:
-    vector<string> readBinaryWatch(int n) {
-        vector<string> ans;
-        int mask = 1 << 10;
-        for(int i = 0; i < mask; ++i) {
-            if(__builtin_popcount(i) == n) {
-                int h = i >> 6;
-                int m = (h << 6)^i;
-                if(h < 12 && m < 60) ans.emplace_back(to_string(h) + ((m < 10) ? ":0" : ":") + to_string(m));
+    vector<string> ans;
+    vector<int> h{1,2,4,8};
+    vector<int> m{1,2,4,8,16,32};
+
+    void bkg(pair<int, int> time, int ind, int turnedOn) {
+        if (turnedOn == 0) {
+
+            ans.emplace_back(to_string(time.first) + (time.second < 10
+                             ? ":0" + to_string(time.second)
+                             : ":" + to_string(time.second)));
+            return;
+        }
+
+        for(int i = ind; i < 10; ++i) {
+            if(i < 4) {
+              time.first +=  h[i];
+              if(time.first < 12) bkg(time, i+1, turnedOn -1);
+              time.first -= h[i];
+            }else{
+                time.second += m[i -4];
+                if(time.second < 60) bkg(time, i+1, turnedOn -1);
+                time.second -= m[i-4];
             }
         }
+        
+    }
+
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        bkg({0,0}, 0, turnedOn);
         return ans;
     }
 };

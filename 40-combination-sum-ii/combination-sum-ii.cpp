46cf54fed411b4e8c0cfa0721e1ce6a6a10125eb
Jollu Emiluulu <zholu8@gmail.com>
2024-06-27 16:54:34 +0300

    Time: 8 ms (28.96%) | Memory: 12.7 MB (39.62%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
-public:
-    void helper(int index, int target, vector<int>&candidates, vector<vector<int>>&ans, vector<int>&ds){
-        //base case
-        //when the combination ds is carrying is a valid one add it into th ans
-        if(target == 0){
-            ans.push_back(ds);
+    vector<vector<int>> ans;
+    vector<int> dp;
+    vector<int> nums;
+
+    void bkg(int ind, int target) {
+        if (0 == target) {
+            ans.emplace_back(dp);
             return;
         }
-        for(int i = index; i< candidates.size(); i++){
-            if(candidates[i] > target) break; // no point of it to add it to the ans if the  candidate at ith element exceeds the target no picking up hence break 
-            if(i  > index && candidates[i] == candidates[i-1])
+        // if(nums[ind] > target) return;
+        for (int i = ind; i < nums.size(); ++i) {
+            if (i > ind && nums[i] == nums[i - 1])
                 continue;
-                //to avoid picking up the same elements and thus avoiding duplicates (no picking of element)
-            ds.push_back(candidates[i]);
-            helper(i+1, target- candidates[i], candidates, ans, ds); //call for next  index element
-            ds.pop_back();
+
+            if (nums[i] > target)
+                break;
+
+            dp.push_back(nums[i]);
+            bkg(i + 1, target - nums[i]);
+            dp.pop_back();
         }
-        
     }
+
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());//sort the given candidates vector 
-        vector<vector<int>>ans;
-        vector<int>ds;
-        helper(0, target, candidates, ans, ds);
+        sort(candidates.begin(), candidates.end());
+        nums = move(candidates);
+        bkg(0, target);
         return ans;
     }
 };

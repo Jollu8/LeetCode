9e582346b6319410cc9f484b22e7a45870a02ce3
Jollu Emiluulu <zholu8@gmail.com>
2024-06-29 00:21:13 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (74.31%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,27 +1,25 @@
 class Solution {
-    string ans, dp;
-    vector<char> dex{'a', 'b', 'c'};
-
-    void bkg(int n, int& k) {
-
-        if (dp.size() == n) {
-            if (--k == 0)
-                ans = dp;
-            return;
-        }
-        for (auto c : dex) {
-            if (dp.empty() || dp.back() != c) {
-                dp.push_back(c);
-                bkg(n, k);
-                dp.pop_back();
-            }
-            if (!k)
-                break;
-        }
-    }
 
 public:
     string getHappyString(int n, int k) {
+        string ans, dp;
+        vector<char> dex{'a', 'b', 'c'};
+        function<void(int, int&)> bkg = [&](int a, int& b) {
+            if (dp.size() == a) {
+                if (--b == 0)
+                    ans = dp;
+                return;
+            }
+            for (auto c : dex) {
+                if (dp.empty() || dp.back() != c) {
+                    dp.push_back(c);
+                    bkg(n, k);
+                    dp.pop_back();
+                }
+                if (!k)
+                    break;
+            }
+        };
         bkg(n, k);
         return ans;
     }

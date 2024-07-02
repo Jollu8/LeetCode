e22729f391fd3d8d732e72a82b680d80e016f8f8
Jollu Emiluulu <zholu8@gmail.com>
2024-07-02 21:49:08 +0300

    Time: 7 ms (60.85%) | Memory: 12.6 MB (37.59%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,21 @@
-int speed = [] {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return 0;
-}();
-
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& g) {
-        vector<vector<int>> ans(g.size()+1, vector<int>(g[0].size()+1, INT_MAX));
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 1e9));
 
-        for(int i = 1; i < ans.size(); ++i) {
-            for(int j = 1; j < ans[0].size(); ++j) {
-                ans[i][j] = g[i-1][j-1];
+        for(int i = 1; i <= n; ++i)
+            for(int j = 1; j <= m; ++j)
+                dp[i][j] = grid[i-1][j-1];
+
+
+        for(int i = 1; i <= n; ++i)
+            for(int j = 1; j <= m; ++j){
+                if(1 == i && 1 == j) continue;
+                dp[i][j] = grid[i-1][j-1] + min(dp[i-1][j], dp[i][j-1]);
             }
-        }
-
-        for(int i = 1; i < ans.size(); ++i) {
-            for(int j = 1; j < ans[0].size(); ++j) {
-                if(i == 1 && j == 1) continue ;
-                ans[i][j] += min(ans[i-1][j], ans[i][j-1]);
-            }
-        }
-
-        return ans[g.size()][g[0].size()];
+        
+        return dp[n][m];
+        
     }
 };

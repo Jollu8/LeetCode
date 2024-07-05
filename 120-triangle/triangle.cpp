4813f5df78abc02a0067c5b8b918653d6a205882
Jollu Emiluulu <zholu8@gmail.com>
2024-07-05 23:44:05 +0300

    Time: 4 ms (73.18%) | Memory: 11.3 MB (58.34%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,11 +1,22 @@
+
 class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& tr) {
-        for(int i = tr.size() - 2; i >= 0; --i) {
-            for(int j = 0; j <= i; ++j) {
-                tr[i][j] += min(tr[i+1][j], tr[i+1][j+1]);
-            }
-        }
-        return tr[0][0];
+    vector<vector<int>> dp, triangle;
+
+    int memo(int i, int j) {
+        if(i == triangle.size()-1) return triangle[i][j];
+        if(dp[i][j] != 1e9) return dp[i][j];
+
+        return dp[i][j] = triangle[i][j] + min(memo(i+1, j), memo(i+1, j+1));
     }
+
+public:
+    int minimumTotal(vector<vector<int>>& tri) { 
+        triangle = move(tri);
+        int m {};
+        for(auto &v : triangle) {
+            m = max(m, (int)v.size());
+        }
+        dp = vector<vector<int>>(m+1, vector<int> (m+1, 1e9));
+        return memo(0,0);
+     }
 };

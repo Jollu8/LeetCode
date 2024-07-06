8b85171696cbf7c1d6e997d45b7789d56eb46ef6
Jollu Emiluulu <zholu8@gmail.com>
2024-07-06 15:29:11 +0300

    Time: 0 ms (100.00%) | Memory: 7.8 MB (17.07%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> vec(m+1, vector<int>(n+1, 0));
-        vec[1][0] = 1;
-        for(auto i = 1; i< m+1; ++i)
-            #pragma omp simd
-            for(auto j = 1; j < n+1; ++j) {
-                vec[i][j] = vec[i-1][j] + vec[i][j-1];
-            }
-        return vec[m][n];
+        vector<vector<int>> dp(m, vector<int>(n,1));
+      
+        for(auto i =1; i < m ; ++i) 
+            for(int j = 1; j < n; ++j) 
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            
+        return dp[m-1][n-1];     
     }
 };

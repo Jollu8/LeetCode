ba2364c37f9362f9686e5e968143cd0c2146cedc
Jollu Emiluulu <zholu8@gmail.com>
2024-08-31 15:20:25 +0300

    Time: 11 ms (47.31%) | Memory: 7.4 MB (65.97%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
-    unordered_map<int, int> memo;
+   
 public:
     int fib(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
-        if(memo.contains(n)) return memo[n];
-        memo[n] = fib(n-1) + fib(n-2);
-        return memo[n];
+        return fib(n-1) + fib(n-2);
     }
 };

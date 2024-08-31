2be8bac628454e820f48e704a4fcf7fb329ede94
Jollu Emiluulu <zholu8@gmail.com>
2024-08-31 16:03:59 +0300

    Time: 3 ms (29.68%) | Memory: 7.7 MB (8.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    unordered_map<int, int>mp;
+    vector<int> memo;
 
     int f(int n) {
         if(n <= 1) return 1;
-        if(mp.contains(n)) return mp[n];
+        if(memo[n] != 0) return memo[n];
 
-        return mp[n] =  f(n-1) + f(n-2);
+        return memo[n] =  f(n-1) + f(n-2);
         
     }
 
 public:
     int climbStairs(int  n) {
+        memo = vector<int>(n+1, 0);
     return f(n);
     }
 };

d4e49de90b4aac2c2de59fea4a77f2b4b5166462
Jollu Emiluulu <zholu8@gmail.com>
2024-08-31 16:21:49 +0300

    Time: 0 ms (100.00%) | Memory: 7.5 MB (59.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,28 @@
 class Solution {
-    vector<int> memo;
 
-    int f(int n) {
-        if(n <= 1) return 1;
-        if(memo[n] != 0) return memo[n];
-
-        return memo[n] =  f(n-1) + f(n-2);
+    void f(int n, vector<int>& memo) {
+        if (n <= 1){
+            return;
+        }
+        if (memo[n] != 0)
+            return ;
         
+        f(n - 1, memo);
+        f(n - 2, memo);
+        memo[n] = memo[n-1] + memo[n-2];
+         
+         
     }
 
 public:
-    int climbStairs(int  n) {
-        memo = vector<int>(n+1, 0);
-    return f(n);
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, 0);
+        memo[0] = 1;
+        memo[1] = 1;
+        f(n, memo);
+        for(auto i : memo) std::cout << i << " ";
+        return memo[n];
+
+
     }
 };

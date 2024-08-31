74772f92594213b8136411b58eb5d9e09d8101c3
Jollu Emiluulu <zholu8@gmail.com>
2024-08-31 15:58:53 +0300

    Time: 0 ms (100.00%) | Memory: 8.2 MB (8.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
+    unordered_map<int, int>mp;
+
+    int f(int n) {
+        if(n <= 1) return 1;
+        if(mp.contains(n)) return mp[n];
+
+        mp[n] =  f(n-1) + f(n-2);
+        return mp[n];
+    }
+
 public:
     int climbStairs(int  n) {
-        if(n <= 3)return n; 
-        unordered_map<int ,int> dp;
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i <n ; ++i) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-     
-        return dp[n-1];
+    return f(n);
     }
 };

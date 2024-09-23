0512666327d949125ab2993e2107a25e9bd52163
Jollu Emiluulu <zholu8@gmail.com>
2024-09-24 00:16:00 +0300

    Time: 258 ms (22.49%) | Memory: 111.2 MB (29.13%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_set<string> st;
+    int dp[51];
+    int solve(string& s, int index) {
+        if (index >= s.size()) {
+            return 0;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int len = INT_MAX;
+        for (int i = 1; i <= s.size(); i++) {
+            string str = s.substr(index, i);
+            if (st.find(str) != st.end()) {
+                len = min(len, solve(s, index + i));
+            }
+        }
+
+        len = min(len, 1 + solve(s, index + 1));
+        return dp[index] = len;
+    }
+    int minExtraChar(string s, vector<string>& d) {
+        for (int i = 0; i < d.size(); i++) {
+            st.insert(d[i]);
+        }
+        memset(dp, -1, sizeof(dp));
+        return solve(s, 0);
+    }
+};

04ba6a560609268385f922c0ca032cb8cd4626b7
Jollu Emiluulu <zholu8@gmail.com>
2024-11-04 14:04:36 +0300

    Time: 60 ms (21.37%) | Memory: 32.3 MB (5.03%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     string compressedString(string s) {
         std::string ans;
-        if (s.empty()) return ans;
-
         int cnt{};
         auto c = s.front();
         for (char i: s) {
             if (c != i ) {
-                if(cnt != 1){
-                    ans += to_string(cnt) + c;
-                }else {
-                    ans += to_string(cnt) + c;
-                }
-                c = i;
+               ans += to_string(cnt) + c ;
+               c = i;
                 cnt = 1;
             }
             else if (cnt + 1 > 9) {

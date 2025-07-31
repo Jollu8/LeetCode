e7ce24843d089f59a253e4e54806fc95c644ae30
Jollu Emiluulu <zholu8@gmail.com>
2025-07-31 15:16:47 +0300

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.43%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         
+        for (int i = 0; i < nums.size(); ++i) {
+            int ind = i;
+
+            for(int j = i+1; j < nums.size(); ++j) {
+                if (nums[i] > nums[j]) {
+                    ind = j;
+                }
+            }
+            if (i != ind) {
+                swap(nums[i], nums[ind]);
+                --i;
+            }
+            
+        }
     }
 };
